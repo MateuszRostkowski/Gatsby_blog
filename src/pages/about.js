0550770ce15336fcb4515ea17522530b48ebeca8
MateuszR <mateusz.rostkowsky995@gmail.com>
2019-09-29 20:56:36 +0200

    added bootstrap and reactstrap, added plugins and created navbar

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+// import Image from "../components/image"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About us..</h1>
+    
+  </Layout>
+)
+
+export default AboutPage
