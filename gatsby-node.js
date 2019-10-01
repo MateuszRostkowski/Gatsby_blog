0f23f15fcc258cf225c681f4808f45ff491e1d14
MateuszR <mateusz.rostkowsky995@gmail.com>
2019-10-01 20:38:49 +0200

    added pagination links to the home page

--- gatsby-node.js
+++ gatsby-node.js
@@ -121,7 +121,8 @@ exports.createPages = ({ actions, graphql }) => {
                 context: {
                     limit: postPerPage,
                     skip: index * postPerPage,
-                    currentPage
+                    currentPage,
+                    numberOfPages
                 }
             })
         })
